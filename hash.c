666cf5535cadee69cca60cfcaa27e2d794a88520
John Tyner <jtyner@gmail.com>
2023-10-24 11:36:37 -0400

    add documentation for hash table

--- hash.c
+++ hash.c
@@ -16,24 +16,44 @@ unsigned long hash_mul(const unsigned long k, const size_t m)
     return (M - (unsigned long)M) * m;
 }
 
+/*!
+ * @private
+ *
+ * Given a hash_node, return the containing object
+ */
 static void * __hash_element(const struct hash * const h,
                              struct hash_node * const n)
 {
     return (void *)((uintptr_t)n - h->off);
 }
 
+/*!
+ * @private
+ *
+ * Given an object, return the contained hash_node
+ */
 static struct hash_node * __hash_node(const struct hash * const h,
                                       const void * const e)
 {
     return (void *)((uintptr_t)e + h->off);
 }
 
+/*!
+ * @private
+ *
+ * Given a key, return the associated hash bucket
+ */
 static struct hash_node ** hash_bucket(
     struct hash * const h, const unsigned long k)
 {
     return &h->b.v[h->hash(k, h->b.n)];
 }
 
+/*!
+ * @private
+ *
+ * Visit each element within a particular bucket in the hash table
+ */
 static int hash_bucket_foreach(
     struct hash * const h, struct hash_node * n,
     cstl_visit_func_t * const visit, void * const p)
@@ -62,83 +82,156 @@ int hash_foreach(struct hash * const h,
     return res;
 }
 
-struct hash_resize_priv
-{
-    struct hash * oh, * nh;
-};
-
+/*! @private */
 static int hash_resize_visit(void * const e, void * const p)
 {
-    struct hash_resize_priv * const hrp = p;
-    const unsigned long k = __hash_node(hrp->oh, e)->k;
+    /* p points to the new hash table */
+    struct hash * const nh = p;
+    /*
+     * even though e is in the old hash table, the @off member
+     * of the new hash table is the same, and it can be used
+     * to extract the hash node and key from the element
+     */
+    const unsigned long k = __hash_node(nh, e)->k;
 
-    hash_erase(hrp->oh, e);
-    hash_insert(hrp->nh, k, e);
+    /*
+     * don't bother removing the element from the old table.
+     * the insertion will overwrite the pointers. it's up to
+     * the caller to clear the buckets, knowing that the
+     * objects contained in them are no longer there.
+     */
+    hash_insert(nh, k, e);
 
     return 0;
 }
 
 void hash_resize(struct hash * const h,
-                 const size_t n, size_t (* hash)(unsigned long, size_t))
+                 struct hash_node ** v, const size_t n,
+                 hash_func_t * const hash)
 {
     if (n > 0) {
-        struct hash_resize_priv hrp;
+        /*
+         * can't DECLARE_HASH because the type
+         * of object is unknown. use the offset
+         * to init
+         */
         struct hash h2;
-        unsigned int i;
-
         hash_init(&h2, h->off);
-        h2.b.v = cstl_malloc(sizeof(*h2.b.v) * n);
+
+        /*
+         * create the buckets. they may have been
+         * supplied by the caller; note that if
+         * necessary
+         */
+        h2.b.ext = (v != NULL);
+        if (h2.b.ext) {
+            h2.b.v = v;
+        } else {
+            h2.b.v = cstl_malloc(sizeof(*h2.b.v) * n);
+        }
+
         if (h2.b.v != NULL) {
+            unsigned int i;
+
+            /*
+             * buckets successfully allocated or supplied
+             * by the caller. initialize them.
+             */
             h2.b.n = n;
-
-            if (hash == NULL) {
-                h2.hash = hash_mul;
-            } else {
-                h2.hash = hash;
-            }
-
-            for (i = 0; i < n; i++) {
+            for (i = 0; i < h2.b.n; i++) {
                 h2.b.v[i] = NULL;
             }
 
-            hrp.oh = h;
-            hrp.nh = &h2;
+            /*
+             * set the hash function in the new hash table.
+             * there is no requirement that it be the same
+             * function as the one in the current table
+             */
+            if (hash != NULL) {
+                h2.hash = hash;
+            } else if (h->hash != NULL) {
+                h2.hash = h->hash;
+            } else {
+                h2.hash = hash_mul;
+            }
 
-            hash_foreach(h, hash_resize_visit, &hrp);
+            /*
+             * walk through all nodes in the current table
+             * and reinsert them into the new table with
+             * the new hash function. keys and everything
+             * else remains the same
+             */
+            hash_foreach(h, hash_resize_visit, &h2);
+
+            /*
+             * the foreach call may not necessarily have
+             * cleanly removed the objects from the existing
+             * hash (for efficiency purposes). this code
+             * needs to clear the buckets so that the clear
+             * function doesn't do anything to the removed
+             * nodes. it's even faster to just set the number
+             * of buckets to zero.
+             */
+            h->b.n = 0;
+            hash_clear(h, NULL);
+
+            /* move the new hash table to the caller's object */
             hash_swap(h, &h2);
-        }
-
-        hash_clear(&h2, NULL);
+        } /*
+           * else, allocation of buckets failed.
+           * nothing to clean up; just exit
+           */
     }
 }
 
 void hash_insert(struct hash * const h,
                  const unsigned long k, void * const e)
 {
-    struct hash_node ** const sn = hash_bucket(h, k);
+    struct hash_node ** const bk = hash_bucket(h, k);
     struct hash_node * const hn = __hash_node(h, e);
 
     hn->k = k;
-    hn->n = *sn;
-    *sn = hn;
+
+    /*
+     * the bucket is a singly-linked/forward list.
+     * insert the new object at the front of the list.
+     */
+    hn->n = *bk;
+    *bk = hn;
 
     h->count++;
 }
 
+/*! @private */
 struct hash_find_priv
 {
+    /* the hash, obviously */
     struct hash * h;
+    /* the key being sought */
     unsigned long k;
-    void * e;
     cstl_const_visit_func_t * visit;
     void * p;
+    /*
+     * initially null, set to the found object
+     * if the visit function indicates that it
+     * is the object being sought
+     */
+    void * e;
 };
 
+/*! @private */
 static int hash_find_visit(void * const e, void * const p)
 {
     struct hash_find_priv * const hfp = p;
 
+    /* only visit nodes with matching keys */
     if (__hash_node(hfp->h, e)->k == hfp->k) {
+        /*
+         * if there is no visit function or the visit function
+         * indicates that this object is the correct one, set
+         * the pointer into the private structure and return
+         * 1 to stop the search
+         */
         if (hfp->visit == NULL || hfp->visit(e, hfp->p) != 0) {
             hfp->e = e;
             return 1;
@@ -155,29 +248,44 @@ void * hash_find(struct hash * const h, const unsigned long k,
 
     hfp.h = h;
     hfp.k = k;
-    hfp.e = NULL;
     hfp.visit = visit;
     hfp.p = p;
+    hfp.e = NULL;
 
     hash_bucket_foreach(h, *hash_bucket(h, k), hash_find_visit, &hfp);
     return hfp.e;
 }
 
+/*! @private */
 struct hash_erase_priv
 {
+    /*
+     * initially a pointer to the bucket. as the
+     * search progresses, this is a pointer to the
+     * pointer to the current object. maintaining
+     * this pointer allows the current object to
+     * be unlinked from the list if/when it's found
+     */
     struct hash_node ** n;
+    /*
+     * a pointer to the object being sought, the
+     * object that will be removed from the hash
+     */
     void * e;
 };
 
+/*! @private */
 static int hash_erase_visit(void * const e, void * const p)
 {
     struct hash_erase_priv * const hep = p;
 
     if (hep->e == e) {
+        /* object found; splice it out of the list */
         *hep->n = (*hep->n)->n;
         return 1;
     }
 
+    /* not found; advance bucket pointer */
     hep->n = &(*hep->n)->n;
     return 0;
 }
@@ -186,19 +294,29 @@ void hash_erase(struct hash * const h, void * const e)
 {
     struct hash_erase_priv hep;
 
+    /*
+     * the object's key determines which
+     * bucket to search for the object
+     */
     hep.n = hash_bucket(h, __hash_node(h, e)->k);
     hep.e = e;
 
+    /*
+     * if the foreach function returns non-zero, then
+     * the object was found and removed.
+     */
     if (hash_bucket_foreach(h, *hep.n, hash_erase_visit, &hep) != 0) {
         h->count--;
     }
 }
 
+/*! @private */
 struct hash_clear_priv
 {
     cstl_clear_func_t * clr;
 };
 
+/*! @private */
 static int hash_clear_visit(void * const e, void * const p)
 {
     struct hash_clear_priv * const hcp = p;
@@ -208,12 +326,23 @@ static int hash_clear_visit(void * const e, void * const p)
 
 void hash_clear(struct hash * const h, cstl_clear_func_t * const clr)
 {
-    struct hash_clear_priv hcp;
+    if (clr != NULL) {
+        struct hash_clear_priv hcp;
 
-    hcp.clr = clr;
-    hash_foreach(h, hash_clear_visit, &hcp);
+        /* call the caller's clear function for each object */
+        hcp.clr = clr;
+        hash_foreach(h, hash_clear_visit, &hcp);
+    }
 
-    cstl_free(h->b.v);
+    if (!h->b.ext) {
+        /*
+         * only free the buckets if they
+         * were allocated internally
+         */
+        cstl_free(h->b.v);
+    }
+
+    h->b.ext = 0;
     h->b.v = NULL;
     h->b.n = 0;
 
@@ -260,10 +389,8 @@ START_TEST(fill)
 {
     static const size_t n = 10;
 
-    struct hash h;
-
-    HASH_INIT(&h, struct integer, n);
-    hash_resize(&h, 32, hash_mul);
+    DECLARE_HASH(h, struct integer, n);
+    hash_resize(&h, NULL, 32, hash_mul);
 
     __test__hash_fill(&h, n);
 
@@ -275,22 +402,21 @@ START_TEST(resize)
     static const size_t n = 100;
     const int i = cstl_rand() % n;
 
-    struct hash h;
+    DECLARE_HASH(h, struct integer, n);
     void * e;
 
-    HASH_INIT(&h, struct integer, n);
-    hash_resize(&h, 16, hash_mul);
+    hash_resize(&h, NULL, 16, hash_mul);
 
     __test__hash_fill(&h, n);
 
     e = hash_find(&h, i, NULL, NULL);
     ck_assert_ptr_ne(e, NULL);
 
-    hash_resize(&h, 9, hash_div);
+    hash_resize(&h, NULL, 9, hash_div);
     ck_assert_uint_eq(hash_size(&h), n);
     ck_assert_ptr_eq(e, hash_find(&h, i, NULL, NULL));
 
-    hash_resize(&h, 23, hash_mul);
+    hash_resize(&h, NULL, 23, hash_mul);
     ck_assert_uint_eq(hash_size(&h), n);
     ck_assert_ptr_eq(e, hash_find(&h, i, NULL, NULL));
 
--- hash.h
+++ hash.h
@@ -5,17 +5,73 @@
 #ifndef CSTL_HASH_H
 #define CSTL_HASH_H
 
+/*!
+ * @defgroup hash Hash table
+ * @ingroup noalloc
+ * @brief A hash table utilizing separate chaining for collision resolution
+ */
+/*!
+ * @addtogroup hash
+ * @{
+ */
+
 #include "common.h"
 
+/*!
+ * @brief Function type for hashing a key into a bucket
+ *
+ * Each element inserted into the hash has an integer associated
+ * with it, aka a "key". This key must be "hashed" in order to fit
+ * into the hash table. The function that does this hashing must
+ * be of this type.
+ *
+ * @param[in] k The key to be hashed
+ * @param[in] m The size of the hash table
+ *
+ * @return A value in the range [0, m)
+ */
+typedef size_t hash_func_t(unsigned long k, size_t m);
+
+/*!
+ * @brief Node to anchor an element within a hash
+ *
+ * Users of the hash object declare this object within another
+ * object as follows:
+ * @code{.c}
+ * struct object {
+ *     ...
+ *     struct hash_node hnode;
+ *     ...
+ * };
+ * @endcode
+ *
+ * When calling hash_init(), the caller passes the offset of @p hnode
+ * within their object as the @p off parameter of that function, e.g.
+ * @code{.c}
+ * offsetof(struct object, hnode)
+ * @endcode
+ */
 struct hash_node
 {
+    /*! @privatesection */
     unsigned long k;
     struct hash_node * n;
 };
 
+/*!
+ * @brief Hash object
+ *
+ * Callers declare or allocate an object of this type to instantiate
+ * a hash. Users are encouraged to declare (and initialize) this
+ * object with the DECLARE_HASH() macro. Any other declaration or
+ * allocation must be initialized via hash_init().
+ */
 struct hash
 {
+    /*! @privatesection */
     struct {
+        /*! @privatesection */
+        int ext;
         struct hash_node ** v;
         size_t n;
     } b;
@@ -23,12 +79,52 @@ struct hash
     size_t count;
     size_t off;
 
-    size_t (* hash)(unsigned long, size_t);
+    hash_func_t * hash;
 };
 
-size_t hash_div(unsigned long, size_t);
-size_t hash_mul(unsigned long, size_t);
+/*!
+ * @brief Constant initialization of a hash object
+ *
+ * @param TYPE The type of object that the hash will hold
+ * @param MEMB The name of the @p hash_node member within @p TYPE.
+ *
+ * @see hash_node for a description of the relationship between
+ *                @p TYPE and @p MEMB
+ */
+#define HASH_INITIALIZER(TYPE, MEMB)            \
+    {                                           \
+        .b = {                                  \
+            .ext = 0,                           \
+            .v = NULL,                          \
+            .n = 0,                             \
+        },                                      \
+        .count = 0,                             \
+        .off = offsetof(TYPE, MEMB),            \
+        .hash = NULL,                           \
+    }
+/*!
+ * @brief (Statically) declare and initialize a hash
+ *
+ * @param NAME The name of the variable being declared
+ * @param TYPE The type of object that the hash will hold
+ * @param MEMB The name of the @p hash_node member within @p TYPE.
+ *
+ * @see hash_node for a description of the relationship between
+ *                @p TYPE and @p MEMB
+ */
+#define DECLARE_HASH(NAME, TYPE, MEMB)                  \
+    struct hash NAME = HASH_INITIALIZER(TYPE, MEMB)
 
+/*!
+ * @brief Initialize a hash object
+ *
+ * @param[in,out] h A pointer to the object to be initialized
+ * @param[in] off The offset of the @p hash_node object within the
+ *                object(s) that will be stored in the hash
+ *
+ * @note The hash object is not ready for use until it has been
+ *       resized via the hash_resize() function
+ */
 static inline void hash_init(struct hash * const h, const size_t off)
 {
     h->b.v = NULL;
@@ -39,30 +135,170 @@ static inline void hash_init(struct hash * const h, const size_t off)
 
     h->hash = NULL;
 }
-#define HASH_INIT(H, TYPE, HN)                  \
-    hash_init(H, offsetof(TYPE, HN))
 
+/*!
+ * @brief Get the number of objects in the hash
+ *
+ * @param[in] h A pointer to the hash
+ *
+ * @return The number of objects in the hash
+ */
 size_t hash_size(const struct hash * const h)
 {
     return h->count;
 }
 
-void hash_resize(struct hash *,
-                 size_t, size_t (* hash)(unsigned long, size_t));
+/*!
+ * @brief Resize the hash table
+ *
+ * @param[in,out] h A pointer to the hash object
+ * @param[in] v A pointer to an array of struct hash_node *. This parameter
+ *              may be NULL, in which case it will be dynamically allocated
+ * @param[in] n The number of elements in the array pointed to by @p v. If
+ *              @p v is NULL, the number of elements to allocate
+ * @param[in] f The function used by this hash object to hash keys. If this
+ *              parameter is NULL, the existing hash function will be reused.
+ *              If there is no existing hash function (because the hash object
+ *              is in an initialized state), the hash_mul() function will be
+ *              used.
+ *
+ * If @p n is zero, this function does nothing. Once this function has been
+ * called, hash_clear() must be called to re/de-initialize the object. If the
+ * function fails, the original hash object is undisturbed.
+ */
+void hash_resize(struct hash * h,
+                 struct hash_node ** v, size_t n,
+                 hash_func_t * f);
 
-void hash_insert(struct hash *, unsigned long, void *);
-void * hash_search(struct hash *, unsigned long,
-                   cstl_const_visit_func_t *, void *);
-void hash_erase(struct hash *, void *);
+/*!
+ * @brief Insert an item into the hash
+ *
+ * @param[in] h A pointer to the hash object
+ * @param[in] k The key associated with the object being inserted
+ * @param[in] e A pointer to the object to insert
+ *
+ * If the caller maintains a pointer to the object or otherwise gets
+ * a pointer via hash_find() or hash_foreach(), the caller may modify
+ * the object as desired. However, the key associated with the object
+ * must not be changed.
+ */
+void hash_insert(struct hash * h, unsigned long k, void * e);
 
-int hash_foreach(struct hash *, cstl_visit_func_t *, void *);
+/*!
+ * @brief Lookup/find a previously inserted object in the hash
+ *
+ * @param[in] h A pointer to the hash object
+ * @param[in] k The key associated with the object being sought
+ * @param[in] visit A pointer to a function that will be called for
+ *                  each object with a matching key. The called function
+ *                  should return a non-zero value when the desired object
+ *                  is found. The pointer may be NULL, in which case, the
+ *                  first object with a matching key will be returned.
+ * @param[in] priv A pointer, belonging to the caller, that will be passed
+ *                 to each invocation of the @p visit function
+ *
+ * @return A pointer to the object that was found
+ * @retval NULL No object with a matching key was found, or the @p visit
+ *              function did not identify a matching object
+ */
+void * hash_find(struct hash * h, unsigned long k,
+                 cstl_const_visit_func_t * visit, void * priv);
 
-static inline void hash_swap(struct hash * const h1, struct hash * const h2)
+/*!
+ * @brief Remove an object from the hash
+ *
+ * @param[in] h A pointer to the hash object
+ * @param[in] e A pointer to the object to be removed. This pointer must
+ *              be to the *actual* object to be removed, not just to an
+ *              object that would compare as equal
+ */
+void hash_erase(struct hash * h , void * e);
+
+/*!
+ * @brief Visit each object within a hash table
+ *
+ * @param[in] h A pointer to the hash object
+ * @param[in] visit A function to be called for each object in the table. The
+ *                  function should return zero to continue visiting objects
+ *                  or a non-zero value to terminate the foreach function.
+ *                  The visit function may alter the object (but not the key)
+ *                  and/or remove the *current* object from the table.
+ * @param[in] priv A pointer, belonging to the caller, that will be passed
+ *                 to each invocation of the @p visit function
+ *
+ * @return The value returned by the last invocation of @p visit or 0
+ */
+int hash_foreach(struct hash * h, cstl_visit_func_t * visit, void * priv);
+
+/*!
+ * @brief Remove all elements from the hash
+ *
+ * @param[in] h A pointer to the hash
+ * @param[in] clr A pointer to a function to be called for each element in
+ *                the hash. The function may be NULL.
+ *
+ * All elements are removed from the hash and the @p clr function is
+ * called for each element that was in the hash. The order in which
+ * the elements are removed and @p clr is called is not specified, but
+ * the callee may take ownership of an element at the time that @p clr
+ * is called for that element and not before.
+ *
+ * Upon return from this function, the hash contains no elements, and
+ * is as it was immediately after being initialized. No further operations
+ * on the hash are necessary to make it ready to go out of scope or be
+ * destroyed.
+ */
+void hash_clear(struct hash * h, cstl_clear_func_t * clr);
+
+/*!
+ * @brief Swap the hash objects at the two given locations
+ *
+ * @param[in,out] a A pointer to a hash
+ * @param[in,out] b A pointer to a(nother) hash
+ *
+ * The hashes at the given locations will be swapped such that upon return,
+ * @p a will contain the hash previously pointed to by @p b and vice versa.
+ */
+static inline void hash_swap(struct hash * const a, struct hash * const b)
 {
     struct hash t;
-    cstl_swap(h1, h2, &t, sizeof(t));
+    cstl_swap(a, b, &t, sizeof(t));
 }
 
-void hash_clear(struct hash *, cstl_clear_func_t *);
+/*!
+ * @name Built-in key hashing functions
+ * @{
+ */
+
+/*!
+ * @brief Hash by division
+ *
+ * The key is hashed by dividing it by the number of buckets in the
+ * table and returning the remainder.
+ *
+ * @param[in] k The key to be hashed
+ * @param[in] m The size of the hash table
+ *
+ * @return A value in the range [0, m)
+ */
+size_t hash_div(unsigned long k, size_t m);
+
+/*!
+ * @brief Hash by multiplication
+ *
+ * The key is hashed by multiplying it by a value @p phi and then
+ * multiplying the fractional portion of that result by @p m. In
+ * this case, the value @p phi is the golden ratio (1.618034), as
+ * suggested by Knuth.
+ *
+ * @param[in] k The key to be hashed
+ * @param[in] m The size of the hash table
+ *
+ * @return A value in the range [0, m)
+ */
+size_t hash_mul(unsigned long k, size_t m);
+/*! @} name */
+
+/*! @} hash */
 
 #endif
