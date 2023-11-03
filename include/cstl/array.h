2ffc5a00ac77029f70fd9135c5b3db836a28b64d
John Tyner <jtyner@gmail.com>
2023-11-02 23:02:55 -0400

    get rid of separate slice object

--- include/cstl/array.h
+++ include/cstl/array.h
@@ -10,49 +10,41 @@
 #include <sys/types.h>
 
 /*!
- * @defgroup array
+ * @defgroup array Array
+ * @ingroup highlevel
+ * @brief Dynamically-allocated, fixed size array
  */
-/*!
- * @defgroup slice
- */
-
-/*!
- * @ingroup array
- */
-typedef struct
-{
-    /*! @privatesection */
-    cstl_shared_ptr_t ptr;
-} cstl_array_t;
-
-/*!
- * @ingroup slice
- */
-typedef struct
-{
-    /*! @privatesection */
-
-    /* measured in number of elements */
-    size_t off, len;
-    cstl_shared_ptr_t ptr;
-} cstl_slice_t;
-
 /*!
  * @addtogroup array
  * @{
  */
 
+typedef struct
+{
+    /*! @privatesection */
+    cstl_shared_ptr_t ptr;
+    /* measured in number of elements */
+    size_t off, len;
+} cstl_array_t;
+
+
 static inline void cstl_array_init(cstl_array_t * const a)
 {
     cstl_shared_ptr_init(&a->ptr);
+    a->off = a->len = 0;
 }
 
-size_t cstl_array_size(const cstl_array_t * a);
+static inline size_t cstl_array_size(const cstl_array_t * const a)
+{
+    return a->len;
+}
+
 void cstl_array_alloc(cstl_array_t * a, size_t nm, size_t size);
 
 static inline void cstl_array_reset(cstl_array_t * const a)
 {
     cstl_shared_ptr_reset(&a->ptr);
+    a->off = a->len = 0;
 }
 
 const void * cstl_array_data_const(const cstl_array_t * a);
@@ -67,52 +59,10 @@ static inline void * cstl_array_at(cstl_array_t * const a, const size_t i)
     return (void *)cstl_array_at_const(a, i);
 }
 
-void cstl_array_slice(cstl_array_t * a,
-                      size_t beg, size_t len,
-                      cstl_slice_t * s);
+void cstl_array_slice(
+    cstl_array_t * a, size_t beg, size_t len, cstl_array_t * s);
 
-/*!
- * @}
- */
-
-/*!
- * @addtogroup slice
- * @{
- */
-
-static inline void cstl_slice_init(cstl_slice_t * const s)
-{
-    s->off = s->len = 0;
-    cstl_shared_ptr_init(&s->ptr);
-}
-
-static inline size_t cstl_slice_size(const cstl_slice_t * const s)
-{
-    return s->len;
-}
-
-static inline void cstl_slice_reset(cstl_slice_t * const s)
-{
-    cstl_shared_ptr_reset(&s->ptr);
-    s->off = s->len = 0;
-}
-
-const void * cstl_slice_data_const(const cstl_slice_t * s);
-static inline void * cstl_slice_data(cstl_slice_t * const s)
-{
-    return (void *)cstl_slice_data_const(s);
-}
-
-const void * cstl_slice_at_const(const cstl_slice_t * s, size_t i);
-static inline void * cstl_slice_at(cstl_slice_t * const s, const size_t i)
-{
-    return (void *)cstl_slice_at_const(s, i);
-}
-
-void cstl_slice_adjust(cstl_slice_t * s, ssize_t beg, size_t len);
-void cstl_slice_slice(const cstl_slice_t * is,
-                      ssize_t beg, size_t len,
-                      cstl_slice_t * os);
+void cstl_array_unslice(cstl_array_t * s, cstl_array_t * a);
 
 /*!
  * @}
--- src/array.c
+++ src/array.c
@@ -17,25 +17,6 @@ struct cstl_raw_array
     /* array follows */
 };
 
-static void * cstl_raw_array_at(const struct cstl_raw_array * const ra,
-                                const size_t i)
-{
-    if (ra == NULL || i >= ra->nm) {
-        abort();
-    }
-    return (void *)((uintptr_t)(ra + 1) + i * ra->sz);
-}
-
-size_t cstl_array_size(const cstl_array_t * const a)
-{
-    const struct cstl_raw_array * const ra =
-        cstl_shared_ptr_get_const(&a->ptr);
-    if (ra != NULL) {
-        return ra->sz;
-    }
-    return 0;
-}
-
 void cstl_array_alloc(cstl_array_t * const a,
                       const size_t nm, const size_t sz)
 {
@@ -48,72 +29,61 @@ void cstl_array_alloc(cstl_array_t * const a,
     if (ra != NULL) {
         ra->sz = sz;
         ra->nm = nm;
+
+        a->len = sz;
     }
 }
 
 const void * cstl_array_data_const(const cstl_array_t * const a)
 {
-    return cstl_raw_array_at(cstl_shared_ptr_get_const(&a->ptr), 0);
+    return cstl_array_at_const(a, 0);
 }
 
 const void * cstl_array_at_const(const cstl_array_t * a, size_t i)
 {
-    return cstl_raw_array_at(cstl_shared_ptr_get_const(&a->ptr), i);
+    if (i > a->len) {
+        abort();
+    } else if (a->len == 0) {
+        return NULL;
+    } else {
+        const struct cstl_raw_array * const ra =
+            cstl_shared_ptr_get_const(&a->ptr);
+
+        return (void *)((uintptr_t)(ra + 1) + (a->off + i) * ra->sz);
+    }
 }
 
 void cstl_array_slice(cstl_array_t * const a,
                       const size_t beg, const size_t len,
-                      cstl_slice_t * const s)
+                      cstl_array_t * const s)
 {
     const struct cstl_raw_array * const ra =
         cstl_shared_ptr_get_const(&a->ptr);
 
-    if (ra == NULL || beg + len > ra->nm) {
+    if (ra == NULL
+        || a->off + beg + len > ra->nm) {
         abort();
     }
 
-    s->off = beg;
+    s->off = a->off + beg;
     s->len = len;
-    cstl_shared_ptr_share(&a->ptr, &s->ptr);
-}
-
-const void * cstl_slice_data_const(const cstl_slice_t * const s)
-{
-    return cstl_raw_array_at(cstl_shared_ptr_get_const(&s->ptr), s->off);
-}
-
-const void * cstl_slice_at_const(const cstl_slice_t * const s, const size_t i)
-{
-    if (i > s->len) {
-        abort();
+    if (a != s) {
+        cstl_shared_ptr_share(&a->ptr, &s->ptr);
     }
-    return cstl_raw_array_at(cstl_shared_ptr_get_const(&s->ptr), s->off + i);
 }
 
-void cstl_slice_adjust(cstl_slice_t * const s,
-                       const ssize_t beg, const size_t len)
+void cstl_array_unslice(cstl_array_t * const s, cstl_array_t * const a)
 {
     const struct cstl_raw_array * const ra =
-        cstl_shared_ptr_get_const(&s->ptr);
-    if (ra == NULL || s->off + beg + len > ra->nm) {
+        cstl_shared_ptr_get_const(&a->ptr);
+    if (ra == NULL) {
         abort();
     }
-    s->off += beg;
-    s->len  = len;
-}
-
-void cstl_slice_slice(const cstl_slice_t * const is,
-                      const ssize_t beg, const size_t len,
-                      cstl_slice_t * const os)
-{
-    const struct cstl_raw_array * const ra =
-        cstl_shared_ptr_get_const(&is->ptr);
-    if (ra == NULL || is->off + beg + len > ra->nm) {
-        abort();
+    a->off = 0;
+    a->len = ra->nm;
+    if (a != s) {
+        cstl_shared_ptr_share(&s->ptr, &a->ptr);
     }
-    cstl_shared_ptr_share(&is->ptr, &os->ptr);
-    os->off = is->off + beg;
-    os->len = len;
 }
 
 #ifdef __cfg_test__
