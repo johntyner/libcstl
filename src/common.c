f478dc9bdf300be0462dbce31446ffa453a2b226
John Tyner <jtyner@gmail.com>
2023-11-03 17:28:46 -0400

    add test for swap function

--- src/common.c
+++ src/common.c
@@ -45,6 +45,7 @@ int cstl_fls(const unsigned long x)
 #ifdef __cfg_test__
 // GCOV_EXCL_START
 #include <check.h>
+#include <stdlib.h>
 
 START_TEST(fls)
 {
@@ -57,6 +58,25 @@ START_TEST(fls)
 }
 END_TEST
 
+START_TEST(swap)
+{
+#define SWAP_TEST(TYPE)                         \
+    do {                                        \
+        TYPE x = rand(), y = rand(), t;         \
+        const TYPE a = x, b = y;                \
+        cstl_swap(&x, &y, &t, sizeof(t));       \
+        ck_assert_mem_eq(&a, &y, sizeof(a));    \
+        ck_assert_mem_eq(&b, &x, sizeof(a));    \
+    } while (0)
+
+    SWAP_TEST(uint8_t);
+    SWAP_TEST(uint16_t);
+    SWAP_TEST(uint32_t);
+    SWAP_TEST(uint64_t);
+
+#undef SWAP_TEST
+}
+
 Suite * common_suite(void)
 {
     Suite * const s = suite_create("common");
@@ -65,6 +85,7 @@ Suite * common_suite(void)
 
     tc = tcase_create("common");
     tcase_add_test(tc, fls);
+    tcase_add_test(tc, swap);
     suite_add_tcase(s, tc);
 
     return s;
