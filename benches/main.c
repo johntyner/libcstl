11adf418b4e4169159c7d81c53b97b80e96980fa
John Tyner <jtyner@gmail.com>
2023-11-24 10:44:24 -0500

    add some benchmarking

--- benches/main.c
+++ benches/main.c
@@ -1,4 +1,70 @@
+#include "internal/bench.h"
+
+#include <stdio.h>
+
+void bench_stop_timer(struct bench_context * const ctx)
+{
+    if (ctx->start.tv_sec >= 0) {
+        struct timespec dur;
+
+        clock_gettime(CLOCK_BENCH, &dur);
+
+        dur.tv_nsec -= ctx->start.tv_nsec;
+        if (dur.tv_nsec < 0) {
+            dur.tv_nsec += 1000000000;
+            dur.tv_sec--;
+        }
+        dur.tv_sec -= ctx->start.tv_sec;
+
+        ctx->accum.tv_nsec += dur.tv_nsec;
+        if (ctx->accum.tv_nsec >= 1000000000) {
+            ctx->accum.tv_nsec -= 1000000000;
+            ctx->accum.tv_sec++;
+        }
+        ctx->accum.tv_sec += dur.tv_sec;
+
+        ctx->start.tv_sec = -1;
+    }
+}
+
+void bench_start_timer(struct bench_context * const ctx)
+{
+    if (ctx->start.tv_sec < 0) {
+        clock_gettime(CLOCK_BENCH, &ctx->start);
+    }
+}
+
+static void bench_run(const char * const name, bench_runner_func_t * const run)
+{
+    DECLARE_BENCH_CONTEXT(ctx);
+    float dur;
+
+    printf("running %-20s", name);
+    fflush(stdout);
+
+    ctx.count = 1;
+    bench_start_timer(&ctx);
+    run(&ctx);
+    bench_stop_timer(&ctx);
+
+    dur = (float)ctx.accum.tv_nsec / 1000000000;
+    dur += ctx.accum.tv_sec;
+
+    printf("%8lu%20.9f s/iter\n", ctx.count, dur / ctx.count);
+}
+
 int main(void)
 {
+#define BENCH_RUN(FUNC)                         \
+    do {                                        \
+        extern bench_runner_func_t FUNC;        \
+        bench_run(#FUNC, FUNC);                 \
+    } while (0)
+
+    BENCH_RUN(bench_qsort);
+    BENCH_RUN(bench_qsort_r);
+    BENCH_RUN(bench_qsort_m);
+    BENCH_RUN(bench_hsort);
+
     return 0;
 }
---
+++ benches/sort.c
@@ -0,0 +1,60 @@
+#include "internal/bench.h"
+
+#include "cstl/vector.h"
+
+#include <stdlib.h>
+
+static int cmp_int(const void * const a, const void * const b, void * const p)
+{
+    (void)p;
+    return *(int *)a - *(int *)b;
+}
+
+static void bench_sort(struct bench_context * const ctx,
+                       const cstl_sort_algorithm_t algo)
+{
+    const unsigned int n = 3271;
+
+    DECLARE_CSTL_VECTOR(v, int);
+    unsigned int i;
+
+    bench_stop_timer(ctx);
+
+    cstl_vector_resize(&v, n);
+
+    for (i = 0; i < ctx->count; i++) {
+        unsigned int j;
+
+        for (j = 0; j < n; j++) {
+            *(int *)cstl_vector_at(&v, j) = rand() % n;
+        }
+
+        bench_start_timer(ctx);
+        __cstl_vector_sort(&v, cmp_int, NULL, cstl_swap, algo);
+        bench_stop_timer(ctx);
+    }
+
+    cstl_vector_clear(&v);
+
+    bench_start_timer(ctx);
+}
+
+void bench_qsort(struct bench_context * const ctx)
+{
+    bench_sort(ctx, CSTL_SORT_ALGORITHM_QUICK);
+}
+
+void bench_qsort_r(struct bench_context * const ctx)
+{
+    bench_sort(ctx, CSTL_SORT_ALGORITHM_QUICK_R);
+}
+
+void bench_qsort_m(struct bench_context * const ctx)
+{
+    bench_sort(ctx, CSTL_SORT_ALGORITHM_QUICK_M);
+}
+
+void bench_hsort(struct bench_context * const ctx)
+{
+    bench_sort(ctx, CSTL_SORT_ALGORITHM_HEAP);
+}
