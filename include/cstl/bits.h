197aae20fdcd85c03751c638be249a0a5f540a66
John Tyner <jtyner@gmail.com>
2023-12-01 11:00:31 -0500

    add bit reflection code

--- include/cstl/bits.h
+++ include/cstl/bits.h
@@ -13,4 +13,17 @@
  */
 int cstl_fls(unsigned long);
 
+/*!
+ * @brief Reverse the bits of an input value
+ *
+ * @return The value of the input with its bits in the reverse order
+ */
+uint8_t cstl_reflect8(uint8_t);
+/*! @copydoc cstl_reflect8 */
+uint16_t cstl_reflect16(uint16_t);
+/*! @copydoc cstl_reflect8 */
+uint32_t cstl_reflect32(uint32_t);
+/*! @copydoc cstl_reflect8 */
+uint64_t cstl_reflect64(uint64_t);
+
 #endif
